d961204905813c163aa59cff507a5a53cc0eecb8
ttus612 <thanhtu6122002@gmail.com>
2023-11-20 11:04:38 +0700

    done

--- src/main/java/vn/edu/iuh/fit/www_lab5_week06_20014641_vonguyenthanhtu/backend/services/CandidateServices.java
+++ src/main/java/vn/edu/iuh/fit/www_lab5_week06_20014641_vonguyenthanhtu/backend/services/CandidateServices.java
@@ -1,13 +1,13 @@
 package vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.backend.models.Candidate;
 import vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.backend.repositories.CandidateRepository;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CandidateServices {
@@ -18,5 +18,20 @@ public class CandidateServices {
         Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
         return candidateRepository.findAll(pageable);//findFirst.../findTop...
+    }
+
+    public Page<Candidate> findPaginated(Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<Candidate> list;
+        List<Candidate> candidates = candidateRepository.findAll();
+        if (candidates.size() < startItem) {
+            list = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, candidates.size());
+            list = candidates.subList(startItem, toIndex);
+        }
+        return new PageImpl<>(list, PageRequest.of(currentPage, pageSize), candidates.size());
     }
 }
--- src/main/java/vn/edu/iuh/fit/www_lab5_week06_20014641_vonguyenthanhtu/frontend/controllers/CandidateController.java
+++ src/main/java/vn/edu/iuh/fit/www_lab5_week06_20014641_vonguyenthanhtu/frontend/controllers/CandidateController.java
@@ -1,11 +1,21 @@
 package vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.frontend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.backend.models.Candidate;
 import vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.backend.repositories.CandidateRepository;
 import vn.edu.iuh.fit.www_lab5_week06_20014641_vonguyenthanhtu.backend.services.CandidateServices;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class CandidateController {
@@ -19,5 +29,15 @@ public class CandidateController {
     public String showCan(Model model){
         model.addAttribute("candidates",  candidateRepository.findAll());
         return "candidates/candidates";
+    }
+
+    @GetMapping("/candidates")
+    public String showCandidateListPaging(Model model,
+                                          @RequestParam("page") Optional<Integer> page,
+                                          @RequestParam("size") Optional<Integer> size) {
+        Pageable pageable = PageRequest.of(page.orElse(0), size.orElse(10));
+        Page<Candidate> candidates = candidateServices.findPaginated(pageable);
+        model.addAttribute("candidates", candidates);
+        return "candidates/candidates-paging";
     }
 }
